bd606fc8ceed62d18c79ab0c977b29be582749f2
Sergey Okatov <sokatov@gmail.com>
2019-08-26 21:12:49 +0500

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='py-video',
+    version='0.0.1',
+    packages=['some'],
+    url='',
+    license='MIT',
+    author='Sergey Okatov',
+    author_email='sokatov@gmail.com',
+    description='TESTS',
+    python_requires='>=3.6, <4',
+    install_requires=[
+        'keras',
+        'opencv-python'
+    ],
+)
